0a44bcbd39e9792ce0f62c42bc1ef106271ecc6f
medicenhoy <emartinezcabello@byupathway.edu>
2026-01-21 07:00:39 -0300

    002

---
+++ scripts/temples.js
@@ -0,0 +1,10 @@
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('open');
+});
+
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
