2a746a544665cd62bea16e1495d983684810d268
medicenhoy <emartinezcabello@byupathway.edu>
2026-01-24 19:41:26 -0300

    005

---
+++ scripts/place.js
@@ -0,0 +1,9 @@
+function calculateWindChill(t, w) {
+    return (13.12 + 0.6215 * t - 11.37 * w ** 0.16 + 0.3965 * t * w ** 0.16).toFixed(1);
+}
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChillElement.textContent = `${calculateWindChill(temperature, windSpeed)} °C`;
+} else {
+    windChillElement.textContent = "N/A";
+}
